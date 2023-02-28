b555a0a77b95b6b7ed3fbc76c0cf2cf33b6ee050
ChaconEfrain <efrachaga@gmail.com>
2023-02-27 19:16:55 -0600

    Script component for cropper

---
+++ src/cropper_script/Script.tsx
@@ -0,0 +1,11 @@
+import Script from "next/script";
+
+const CropperScript = () => {
+  return (
+    <>
+      <Script src="/path/to/cropper.js" />
+    </>
+  );
+};
+
+export default CropperScript;
